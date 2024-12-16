c96613dde4a7ba4af9305768a0f0abfbdcf3b333
Kevin Whitley <kevinwhitley@Kevins-Mac-mini.local>
2024-12-15 22:28:28 -0600

    initial commit

--- src/datePlus.ts
+++
@@ -1,5 +0,0 @@
-import { ms } from './ms'
-
-// FUNCTION: get future date from a duration string (e.g. datePlus('3 hours'))
-export const datePlus = (duration: string | number, from = new Date): Date =>
-  new Date(from.getTime() + ms(duration))
--- src/seconds.ts
+++
@@ -1,4 +0,0 @@
-import { ms } from './ms'
-
-export const seconds = (duration: string | number): number =>
-  ms(duration) / 1000
